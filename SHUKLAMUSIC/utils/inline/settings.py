3e0ec161b0e6a9b5dd1e1706895fd72a99e7ff0e
atrinatrin <poyahashti@gmail.com>
2024-12-26 19:01:15 +0330

    Update settings.py

--- SHUKLAMUSIC/utils/inline/settings.py
+++ SHUKLAMUSIC/utils/inline/settings.py
@@ -1,77 +1,72 @@
 from typing import Union
-
 from pyrogram.types import InlineKeyboardButton
-
 
 def setting_markup(_):
     buttons = [
         [
-            InlineKeyboardButton(text=_["ST_B_1"], callback_data="AU"),
-            InlineKeyboardButton(text=_["ST_B_3"], callback_data="LG"),
+            InlineKeyboardButton(text="کاربران مجاز ⊱", callback_data="AU"),
+            InlineKeyboardButton(text="زبان ⊱", callback_data="LG"),
         ],
         [
-            InlineKeyboardButton(text=_["ST_B_2"], callback_data="PM"),
+            InlineKeyboardButton(text="حالت پخش ⊱", callback_data="PM"),
         ],
         [
-            InlineKeyboardButton(text=_["ST_B_4"], callback_data="VM"),
+            InlineKeyboardButton(text="تنظیمات رأی ⊱", callback_data="VM"),
         ],
         [
-            InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close"),
+            InlineKeyboardButton(text="⊝ بستن", callback_data="close"),
         ],
     ]
     return buttons
-
 
 def vote_mode_markup(_, current, mode: Union[bool, str] = None):
     buttons = [
         [
-            InlineKeyboardButton(text="Vᴏᴛɪɴɢ ᴍᴏᴅᴇ ➜", callback_data="VOTEANSWER"),
+            InlineKeyboardButton(text="⊱ حالت رأی گیری", callback_data="VOTEANSWER"),
             InlineKeyboardButton(
-                text=_["ST_B_5"] if mode == True else _["ST_B_6"],
+                text="✓ فعال" if mode == True else "✗ غیرفعال",
                 callback_data="VOMODECHANGE",
             ),
         ],
         [
-            InlineKeyboardButton(text="-2", callback_data="FERRARIUDTI M"),
+            InlineKeyboardButton(text="⊖", callback_data="FERRARIUDTI M"),
             InlineKeyboardButton(
-                text=f"ᴄᴜʀʀᴇɴᴛ : {current}",
+                text=f"مقدار فعلی: {current}",
                 callback_data="ANSWERVOMODE",
             ),
-            InlineKeyboardButton(text="+2", callback_data="FERRARIUDTI A"),
+            InlineKeyboardButton(text="⊕", callback_data="FERRARIUDTI A"),
         ],
         [
             InlineKeyboardButton(
-                text=_["BACK_BUTTON"],
+                text="⊲ برگشت",
                 callback_data="settings_helper",
             ),
-            InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close"),
+            InlineKeyboardButton(text="⊝ بستن", callback_data="close"),
         ],
     ]
     return buttons
-
 
 def auth_users_markup(_, status: Union[bool, str] = None):
     buttons = [
         [
-            InlineKeyboardButton(text=_["ST_B_7"], callback_data="AUTHANSWER"),
+            InlineKeyboardButton(text="⊱ کاربران مجاز", callback_data="AUTHANSWER"),
             InlineKeyboardButton(
-                text=_["ST_B_8"] if status == True else _["ST_B_9"],
+                text="✓ فعال" if status == True else "✗ غیرفعال",
                 callback_data="AUTH",
             ),
         ],
         [
-            InlineKeyboardButton(text=_["ST_B_1"], callback_data="AUTHLIST"),
+            InlineKeyboardButton(text="⊱ لیست کاربران مجاز", callback_data="AUTHLIST"),
         ],
         [
             InlineKeyboardButton(
-                text=_["BACK_BUTTON"],
+                text="⊲ برگشت",
                 callback_data="settings_helper",
             ),
-            InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close"),
+            InlineKeyboardButton(text="⊝ بستن", callback_data="close"),
         ],
     ]
     return buttons
-
 
 def playmode_users_markup(
     _,
@@ -81,32 +76,32 @@ def playmode_users_markup(
 ):
     buttons = [
         [
-            InlineKeyboardButton(text=_["ST_B_10"], callback_data="SEARCHANSWER"),
+            InlineKeyboardButton(text="⊱ حالت جستجو", callback_data="SEARCHANSWER"),
             InlineKeyboardButton(
-                text=_["ST_B_11"] if Direct == True else _["ST_B_12"],
+                text="✓ مستقیم" if Direct == True else "✗ درون خطی",
                 callback_data="MODECHANGE",
             ),
         ],
         [
-            InlineKeyboardButton(text=_["ST_B_13"], callback_data="AUTHANSWER"),
+            InlineKeyboardButton(text="⊱ حالت گروه", callback_data="AUTHANSWER"),
             InlineKeyboardButton(
-                text=_["ST_B_8"] if Group == True else _["ST_B_9"],
+                text="✓ فعال" if Group == True else "✗ غیرفعال",
                 callback_data="CHANNELMODECHANGE",
             ),
         ],
         [
-            InlineKeyboardButton(text=_["ST_B_14"], callback_data="PLAYTYPEANSWER"),
+            InlineKeyboardButton(text="⊱ نوع پخش", callback_data="PLAYTYPEANSWER"),
             InlineKeyboardButton(
-                text=_["ST_B_8"] if Playtype == True else _["ST_B_9"],
+                text="✓ فعال" if Playtype == True else "✗ غیرفعال",
                 callback_data="PLAYTYPECHANGE",
             ),
         ],
         [
             InlineKeyboardButton(
-                text=_["BACK_BUTTON"],
+                text="⊲ برگشت",
                 callback_data="settings_helper",
             ),
-            InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close"),
+            InlineKeyboardButton(text="⊝ بستن", callback_data="close"),
         ],
     ]
     return buttons
